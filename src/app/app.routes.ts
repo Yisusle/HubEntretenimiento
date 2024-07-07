79e9e5660773aa38e4b3be3135ff1ca07b705f9d
Jesus Leyva <jesusle116@gmail.com>
2024-07-06 19:10:01 -0700

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'auth/login', component:LoginComponent},
+    {path:'movies', component:MovieListComponent},
+    {path:'',redirectTo:'/auth/login', pathMatch: 'full'}
+];
