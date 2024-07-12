2ab2ff41205e3070ac5cd9793e813ec380788c62
Jesus Leyva <jesusle116@gmail.com>
2024-07-12 13:25:25 -0700

    Update testing

--- src/app/modules/auth/pages/login-page/login.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login.component.spec.ts
@@ -20,4 +20,19 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
+  it('should alert if sign in credentials are missing', () => {
+    spyOn(window, 'alert');
+    component.signinEmail = '';
+    component.signinPassword = '';
+    component.onSignIn();
+    expect(window.alert).toHaveBeenCalledWith('Correo o contraseña incorrectos');
+  });
+
+  it('should set isSignIn to true when showSignIn is called', () => {
+    component.showSignUp();
+    component.showSignIn();
+    expect(component.isSignIn).toBe(true);
+  });
 });
